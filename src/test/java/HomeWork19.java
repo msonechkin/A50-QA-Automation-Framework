c8bbc19351a9a8710fd3f1f54fba77800adaa734
msonechkin <mark.sonechkin@gmail.com>
2023-12-12 08:43:59 -0600

    HomeWork17

--- src/test/java/HomeWork19.java
+++ src/test/java/HomeWork19.java
@@ -1,27 +1,43 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class HomeWork19 extends BaseTest{
+public class HomeWork19 extends BaseTest {
 
     @Test
     public void deletePlaylist() throws InterruptedException {
         logIn("andrii.banak@testpro.io", "OknwxILOM2B3$");
         createPlaylist(playListName);
+        //playListName = UUID.randomUUID().toString();
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(text(), '"+playListName+"')]")));
-        WebElement playList = getDriver().findElement(By.xpath("//a[contains(text(), '"+playListName+"')]"));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(text(), '" + playListName + "')]")));
+        WebElement playList = getDriver().findElement(By.xpath("//a[contains(text(), '" + playListName + "')]"));
         playList.click();
+        //click on a playlist
 
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class='del btn-delete-playlist']")));
         getDriver().findElement(By.xpath("//button[@class='del btn-delete-playlist']")).click();
+        //delete a playlist
 
-        //Assert.assertFalse(playList.isDisplayed());
+        wait.until(ExpectedConditions.invisibilityOf(playList));
 
+
+        isVisible(playList);
+
+
+    }
+
+    public boolean isVisible(WebElement element) {
+        try {
+            element.isDisplayed();
+        } catch (StaleElementReferenceException e) {
+            return false;
+        }
+        return true;
     }
 }
