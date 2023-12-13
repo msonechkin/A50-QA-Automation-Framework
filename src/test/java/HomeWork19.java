6bd84fa55ca27d9a3cb75e3d74339b8e12e59bf9
msonechkin <mark.sonechkin@gmail.com>
2023-12-13 01:00:17 -0600

    HomeWork17

--- src/test/java/HomeWork19.java
+++ src/test/java/HomeWork19.java
@@ -5,23 +5,18 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class HomeWork19 extends BaseTest {
-    WebElement playList;
 
     @Test
     public void deletePlaylist() throws InterruptedException {
         logIn("andrii.banak@testpro.io", "OknwxILOM2B3$");
         createPlaylist(playListName);
-        //playListName = UUID.randomUUID().toString();
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(text(), '" + playListName + "')]")));
-        playList = getDriver().findElement(By.xpath("//a[contains(text(), '" + playListName + "')]"));
+        WebElement playList = getDriver().findElement(By.xpath("//a[contains(text(), '" + playListName + "')]"));
         playList.click();
-        //click on a playlist
-
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class='del btn-delete-playlist']")));
         getDriver().findElement(By.xpath("//button[@class='del btn-delete-playlist']")).click();
-        //delete a playlist
 
         wait.until(ExpectedConditions.invisibilityOf(playList));
         Assert.assertEquals(getDriver().findElements(By.xpath("//a[contains(text(), '" + playListName + "')]")).size(), 0);
