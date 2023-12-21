b98f0d0d38cb2dbdd1ac0f02936b5e0ba93f34c6
msonechkin <mark.sonechkin@gmail.com>
2023-12-21 09:22:37 -0600

    PT

--- src/test/java/com/qa/koel/BaseTest.java
+++ src/test/java/com/qa/koel/BaseTest.java
@@ -18,30 +18,32 @@ import org.testng.annotations.Parameters;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.time.Duration;
+import java.util.HashMap;
 
 public class BaseTest {
     private WebDriver driver;
+    private static final ThreadLocal<WebDriver> THREAD_LOCAL = new ThreadLocal<>();
     Actions action;
 
 
     @BeforeMethod
     @Parameters({"BaseURL"})
     public void setUpDriverAndOpen(String BaseURL) throws MalformedURLException {
-        driver = pickDriver(System.getProperty("browserName"));
-        driver.manage().window().maximize();
-        driver.get(BaseURL);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(6));
-        action = new Actions(driver);
+        THREAD_LOCAL.set(pickDriver(System.getProperty("browserName")));
+        THREAD_LOCAL.get().manage().window().maximize();
+        THREAD_LOCAL.get().get(BaseURL);
+        THREAD_LOCAL.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(6));
+        action = new Actions(THREAD_LOCAL.get());
 
     }
 
     public WebDriver getDriver() {
-        return driver;
+        return THREAD_LOCAL.get();
    }
 
     @AfterMethod
     public void closeDriver() {
-        driver.quit();
+        THREAD_LOCAL.get().quit();
     }
 
     public WebDriver pickDriver(String browserName) throws MalformedURLException {
@@ -54,7 +56,7 @@ public class BaseTest {
             case "chrome":
                 WebDriverManager.chromedriver().setup();
                 ChromeOptions options = new ChromeOptions();
-                options.addArguments("--remote-allow-origins=*");//      Added ChromeOptions argument below to fix websocket error
+                options.addArguments("--remote-allow-origins=*");  //Added ChromeOptions argument below to fix websocket error
                 return new ChromeDriver(options);
             case "safari":
                 WebDriverManager.safaridriver().setup();
@@ -67,14 +69,29 @@ public class BaseTest {
             case "grid-chrome":
                 capabilities.setCapability("browserName", "chrome");
                 return  driver = new RemoteWebDriver(URI.create(gridUrl).toURL(), capabilities);
-            case "grid-edge":
-                capabilities.setCapability("browserName", "edge");
-                return  driver = new RemoteWebDriver(URI.create(gridUrl).toURL(), capabilities);
+            case "lambda-driver":
+                return  getLambdaDriver();
             default:
                 WebDriverManager.safaridriver().setup();
                 return new SafariDriver();
         }
     }
 
+    public  WebDriver getLambdaDriver() throws MalformedURLException {
+        String userName = "mark.sonechkin";
+        String authKey = "1vrTtjvjY289FiYdsXVSgiAhxW9r1v6lWFszc2JeSof53m6kqH";
+        String hub = "@hub.lambdatest.com/wd/hub";
 
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("browserName", "Chrome");
+        capabilities.setCapability("browserVersion", "121.0");
+        HashMap<String, Object> ltOptions = new HashMap<>();
+        ltOptions.put("username", userName);
+        ltOptions.put("accessKey", authKey);
+        ltOptions.put("platformName", "Windows 10");
+        ltOptions.put("project", "Koel");
+        capabilities.setCapability("LT:Options", ltOptions);
+        return  new RemoteWebDriver(URI.create("https://" + userName + ":" + authKey + hub).toURL(), capabilities);
+    }
 }
